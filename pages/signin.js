5c5054b4aac354e963c65b6d2e58c6eec54bb8d7
Mateus Tavares <mateustavarestv@hotmail.com>
2021-09-22 10:16:47 -0300

    arrumei o erro agora posso ussar specing, era so a versao do material-ui

--- pages/signin.js
+++ pages/signin.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/styles'
 
 
 const useStyles = makeStyles((theme) =>({
-    root: {
+  root: {
     height: '100vh',
   },
   image: {
@@ -25,23 +25,23 @@ const useStyles = makeStyles((theme) =>({
     backgroundPosition: 'center',
   },
   paper: {
-    margin: 10,
+    margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
   avatar: {
-    margin: 5,
-    backgroundColor: '#000000',
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: 7,
+    marginTop: theme.spacing(1),
   },
   submit: {
-    margin: 8,
+    margin: theme.spacing(3, 0, 2),
   },
-}));
+}))
 
 export default function SignInSide() {
   const classes = useStyles()
@@ -107,5 +107,5 @@ export default function SignInSide() {
         </div>
       </Grid>
     </Grid>
-  );
+  )
 }
