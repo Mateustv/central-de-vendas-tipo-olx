eda86f5d9c31bba6c82634bd4bc07dc3514ddc61
Mateus T <mateustavarestv@hotmail.com>
2021-10-20 15:46:04 -0300

    criando o metodo post da API

---
+++ src/models/users.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema({
+  name: {
+    type: "string",
+    required: [true, 'O campo "nome" é obrigatório']
+  },
+  password: {
+    type: "string",
+    required: [true, 'O campo "senha" é obrigatório']
+  },
+  email: {
+    type: "string",
+    required: [true, 'O campo "E-mail" é obrigatório']
+  },
+})
+
+export default mongoose.model.users || mongoose.model('users', schema)
